8ffc4b500a1e5e7515f34b59d5f9e96597a12ca1
Tiago Cabral <tiagocabral@cjr.org.br>
2017-06-15 12:22:49 -0300

    Adicionado entidade usuário

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ end
 gem 'rails', '~> 5.1.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
+# Gema para o bcrypt
+gem 'bcrypt', '~> 3.1.7'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+   has_secure_password
+
+   validates :username, presence: true, # deve ser diferente de uma string vazia
+                        uniqueness: {case_sensitive: false}, # deve ser único, independente de letra maiúscula ou mínuscula
+                        length: {minimum: 5, maximum: 25}
+
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  # expressão regular para determinar a validade do email
+
+   validates :email, presence: true,
+                     length: {maximum: 110},
+                     format: { with: VALID_EMAIL_REGEX }
+end
