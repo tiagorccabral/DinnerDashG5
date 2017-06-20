61d6f0925eb7b214a0797fef93bf229dd8ba1b72
Tiago Cabral <tiagocabral@cjr.org.br>
2017-06-20 13:13:12 -0300

    Adicionado controladoras, models de categorias

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
   resources :items
-
+  resources :categories
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#delete'
-  
+
   root 'welcome#home'
 end
