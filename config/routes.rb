fd926c73b3e08f6dac09587b184740411e0dd8cc
Tiago Cabral <tiagocabral@cjr.org.br>
2017-06-22 15:56:53 -0300

    Logout adicionado

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#delete'
+  delete 'logout', to: 'sessions#destroy'
 
   root 'welcome#home'
 end
