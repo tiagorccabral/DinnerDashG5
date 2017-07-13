2c6aca416740ef12859162c4f77a8eb7161234fe
Tiago Cabral <tiagocabral@cjr.org.br>
2017-07-12 23:22:54 -0300

    Adicionado botão comprar (pre-pre-alpha), metodos de order, pagina de pedidos na nav bar

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,20 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+    @user = current_user
+  end
   def new
   end
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to items_path
+    end
+  end
+  def show
+  end
+  private
+    def order_params
+      params.require(:order).permit(:value,:amount,:user_id)
+    end
 end
